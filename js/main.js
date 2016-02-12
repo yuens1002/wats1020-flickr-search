db716f4573ae4237424a7b686fa5067433bd923e
sunny yuen <yuens1002@gmail.com>
2016-02-12 13:48:19 -0800

    bug squashing
    fixed issue with msnry.loaded() not working after the initial load.
    aesthetic changes to page and data formatting to .items.

--- js/main.js
+++ js/main.js
@@ -7,126 +7,73 @@
 // Allow users to click the images to see a larger version with more information.
 $(document).on('ready', function() {
 	
-	/* var $grid = $('.grid').masonry({
-	  itemSelector: '.grid-item',
-	  percentPosition: true,
-	  columnWidth: '.grid-sizer'
-	});
-	// layout Isotope after each image loads
-	$grid.imagesLoaded().progress( function() {
-	  $grid.masonry();
-	}); */ 
+	//format date to d.m.y from data.date_taken
+	var formatDate = function(unformattedDate) {
+		var formattedDate = new Date(unformattedDate);
+		var d = formattedDate.getDate();
+		var m =  formattedDate.getMonth();
+		m += 1;  // JavaScript months are 0-11
+		var y = formattedDate.getFullYear();
+		return (m + "." + d + "." + y);
+	};
+	
+	//slice out the extras from data.author
+	var formatAuthor = function(string) {
+    	var frist = string.search(/\(/);
+    	var last = string.search(/\)/);
+    	return string.slice(frist+1, last);
+	}
 
 	// processes/sends user search terms for a flickr api response 
 	var searchImages = function(tags) {
+			//appendTo text to keyword id in DOM
+			
 			var flickrAPI = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
 			$.getJSON(flickrAPI, {
 				// to test w/o user input (replace 'setattle w/ tags) //
 				tags: tags,
 				tagmode: 'any',
 				format: 'json'
-			})
-			
-			.done(function(data) {
-				// try to just display the 1st image that it finds //
-				/* $(data.items[0].('<img>').attr('src', data.items.media.m).appendTo('#images');  */
+			}).done(function(data) {
 				//empties image area first
 				$('#images').empty();
-				$('<div class="grid-sizer col-xs-4 col-sm-3 col-md-2"></div>').appendTo('#images');
-				$.each(data.items, function(i, item) {
-					
+				$('<p class="lead">Showing Images of <strong id="keyword"></strong>').appendTo('#images');
+				$('#keyword').text(tags);
+		 		// inserts this to create a newImages container each time masonry is called
+				$('<div id="newImages">').appendTo('#images');
+				$.each(data.items, function(i, item) {				
 					var outer = $('<div class="grid-item col-xs-4 col-sm-3 col-md-2">');
 					var inner = $('<div class="grid-item-content">').appendTo(outer);
-					$('<p>').addClass('text-capitalize').html(item.title).appendTo(inner);
+					
 					var image = $('<img>').attr('src', item.media.m).addClass('img-responsive');
 					$('<a>').attr('href', item.link).html(image).appendTo(inner);
-					$('<p>').text(item.date_taken).appendTo(inner);
-					$('<p>').html(item.author).appendTo(inner);
-					outer.appendTo('#images');
-					if(i === 24) {
+					$('<p>').addClass('text-capitalize').html('<strong>' + item.title + '</strong>').appendTo(inner);
+					$('<p>').html(formatDate(item.date_taken) + '<br>' + formatAuthor(item.author)).appendTo(inner);
+					outer.appendTo('#newImages').imagesLoaded().progress( function() {
+				 		$('#newImages').masonry();
+					});
+					
+					if(i === 17) {
 						return false;
 					}
-			
+					
 				});
-				
-				$('#images').imagesLoaded( function() {
-					$('#images').masonry();
-				});
+
 			});
 	};
 	
 	$('button.search').click(function() {
 		// not sure why preventDefaualt is necessary
-		event.preventDefault();
+		// event.preventDefault();
 		// $("input[name=nameGoesHere]").val();
 		var tags = $('input[name=searchText]').val();
 		searchImages(tags);
 	
 	});
 	
-	
-	/*
-	
-	<script>
-(function() {
-  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  $.getJSON( flickerAPI, {
-    tags: "mount rainier",
-    tagmode: "any",
-    format: "json"
-  })
-    .done(function( data ) {
-      $.each( data.items, function( i, item ) {
-        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-        if ( i === 3 ) {
-          return false;
-        }
-      });
-    });
-})();
-</script>
-	
-	*/
-	
-    // Place your code here, inside the document ready handler.
-
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
-
-    // Inside the `searchImages()` function, the following things should happen:
-
-        // 1.   Accept a string value called `tags` as an argument. Example:
-        //      `var searchPhotos = function(tags){`
-        //
-        // 2.   Define the location of the Flickr API like this:
-        //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
-        //
-        // 3.   Construct a `$.getJSON()` call where you send a request object
-        //      including the tags the user submitted, and a `done()` handler
-        //      that displays and refreshes the content appropriately.
-        //
-        // 4.   Update the display to add the images to the list with the id
-        //      `#images`.
-
-    // Attach an event to the search button (`button.search`) to execute the
-    // search when clicked.
-
-        // When the Search button is clicked, the following should happen:
-        //
-        // 1.   Prevent the default event execution so the browser doesn't
-        //      Example: `event.preventDefault();`
-        //
-        // 2.   Get the value of the 'input[name="searchText"]' and use that
-        //      as the `tags` value you send to `searchImages()`.
-        //
-        // 3.   Execute the `searchImages()` function to fetch images for the
-        //      user.
-
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-
+	//showing default search seattle when page loads
+	var searchword = 'Seattle';
+	searchImages(searchword);
 
 });
 
