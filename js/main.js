b3fc28f2dc19b5cdba3b4ebe0c0b92f2f263b9ee
sunny yuen <yuens1002@gmail.com>
2016-02-11 21:58:23 -0800

    completed assignment per basic requirements
    have an issue with masonry not working after the 1st search.  would
    like to work on the aesthetics of the site.  a default search to
    display,  style the return of the data, reg replace on the author
    string to only display the author would be nice.  modal hasn’t been
    implemented yet.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,89 @@
 // of tags and view the images that are found.
 //
 // Allow users to click the images to see a larger version with more information.
-$(document).on('ready', function(){
+$(document).on('ready', function() {
+	
+	/* var $grid = $('.grid').masonry({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  columnWidth: '.grid-sizer'
+	});
+	// layout Isotope after each image loads
+	$grid.imagesLoaded().progress( function() {
+	  $grid.masonry();
+	}); */ 
+
+	// processes/sends user search terms for a flickr api response 
+	var searchImages = function(tags) {
+			var flickrAPI = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+			$.getJSON(flickrAPI, {
+				// to test w/o user input (replace 'setattle w/ tags) //
+				tags: tags,
+				tagmode: 'any',
+				format: 'json'
+			})
+			
+			.done(function(data) {
+				// try to just display the 1st image that it finds //
+				/* $(data.items[0].('<img>').attr('src', data.items.media.m).appendTo('#images');  */
+				//empties image area first
+				$('#images').empty();
+				$('<div class="grid-sizer col-xs-4 col-sm-3 col-md-2"></div>').appendTo('#images');
+				$.each(data.items, function(i, item) {
+					
+					var outer = $('<div class="grid-item col-xs-4 col-sm-3 col-md-2">');
+					var inner = $('<div class="grid-item-content">').appendTo(outer);
+					$('<p>').addClass('text-capitalize').html(item.title).appendTo(inner);
+					var image = $('<img>').attr('src', item.media.m).addClass('img-responsive');
+					$('<a>').attr('href', item.link).html(image).appendTo(inner);
+					$('<p>').text(item.date_taken).appendTo(inner);
+					$('<p>').html(item.author).appendTo(inner);
+					outer.appendTo('#images');
+					if(i === 24) {
+						return false;
+					}
+			
+				});
+				
+				$('#images').imagesLoaded( function() {
+					$('#images').masonry();
+				});
+			});
+	};
+	
+	$('button.search').click(function() {
+		// not sure why preventDefaualt is necessary
+		event.preventDefault();
+		// $("input[name=nameGoesHere]").val();
+		var tags = $('input[name=searchText]').val();
+		searchImages(tags);
+	
+	});
+	
+	
+	/*
+	
+	<script>
+(function() {
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+})();
+</script>
+	
+	*/
+	
     // Place your code here, inside the document ready handler.
 
     // Create a function called `searchImages()`. This function will handle the
@@ -47,3 +129,4 @@ $(document).on('ready', function(){
 
 
 });
+
